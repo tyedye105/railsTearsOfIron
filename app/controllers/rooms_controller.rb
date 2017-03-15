615e61ca8add33589c5a0c15149d1a2f06c83a29
Marty Kovach <tyedye105@yahoo.com>
2017-03-14 18:50:24 -0700

    add create and edit forms for rooms.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -23,6 +23,30 @@ class RoomsController < ApplicationController
         end
       end
     end
-
   end
+  def new
+    @room = Room.new
+  end
+  def create
+    @room = Room.new(room_params)
+      if @room.save
+        redirect_to rooms_path
+      end
+  end
+  def edit
+    @room = Room.find(params[:id])
+  end
+  def update
+    @room = Room.find(params[:id])
+      if @room.update(room_params)
+        redirect_to rooms_path
+  end
+end
+
+private
+
+  def room_params
+    params.require(:room).permit(:name, :door_ways, :first_description, :normal_description)
+  end
+
 end
