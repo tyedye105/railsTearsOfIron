26a1df60e5eeb8cb5b5d8466ede80cfafe4f7772
Marty Kovach <tyedye105@yahoo.com>
2017-05-19 08:34:56 -0700

    add new tile assests

--- app/controllers/door_controller.rb
+++ app/controllers/door_controller.rb
@@ -13,7 +13,9 @@ class DoorController < ApplicationController
     @double_door = Door.find(@door.id-1)
     @consumable = @character.items.find_by(:name => @door.req_item)
       if @door.update(door_params)
-        @consumable.consume_item
+        if @consumable != nil
+          @consumable.consume_item
+        end
         if @double_door != nil
         @double_door.update(door_params)
         end
